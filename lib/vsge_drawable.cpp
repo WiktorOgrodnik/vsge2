5a0f10c60371b50d005d69e0802e7e2a82e002fe
Wiktor Ogrodnik <wiktor2800-o@outlook.com>
2022-09-10 22:18:22 +0200

    Adds more rectangle essential function, like setSize or some getters

--- lib/vsge_drawable.cpp
+++ lib/vsge_drawable.cpp
@@ -22,7 +22,7 @@ namespace vsge {
 		window->draw(*shape);
 	}
 
-	Vector2f Internal_Shape::getPosition() {
+	Vector2f Internal_Shape::getPosition() const {
 		sf::Vector2f pos = shape->getPosition();
 		return Vector2f(pos.x, pos.y);
 	}
@@ -31,12 +31,36 @@ namespace vsge {
 		shape->setPosition(sf::Vector2f(pos.x, pos.y));
 	}
 
+	Color Internal_Shape::getColor() const {
+		const sf::Color col = shape->getFillColor();
+		return Color(col.r, col.g, col.b, col.a);
+	}
+
 	void Internal_Shape::setColor(Color color) {
 		shape->setFillColor(sf::Color(color.r, color.g, color.b, color.a));
 	}
 
+	Color Internal_Shape::getOutlineColor() const {
+		const sf::Color col = shape->getOutlineColor();
+		return Color(col.r, col.g, col.b, col.a);
+	}
+
+	void Internal_Shape::setOutlineColor(Color color) {
+		shape->setOutlineColor(sf::Color(color.r, color.g, color.b, color.a));
+	}
+
 	Internal_Rectangle::Internal_Rectangle() {
-		shape = new sf::RectangleShape(sf::Vector2f(100, 100));
-		shape->setFillColor(sf::Color::Black);
+		shape = new sf::RectangleShape();
+		this->setSize(Vector2f(100, 100));
+		this->setColor(Color(0, 0, 0));
+	}
+
+	Vector2f Internal_Rectangle::getSize() const {
+		sf::Vector2f size = static_cast<sf::RectangleShape*>(shape)->getSize();
+		return Vector2f(size.x, size.y);
+	}
+
+	void Internal_Rectangle::setSize(Vector2f pos) {
+		static_cast<sf::RectangleShape*>(shape)->setSize(sf::Vector2f(pos.x, pos.y));
 	}
 }
